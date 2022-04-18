e27fec9fb7a1c415c438a61654cece5eaae229a0
arzhangv <34948438+arzhangv@users.noreply.github.com>
2022-04-18 04:33:12 -0700

    Update routes.swift

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -19,14 +19,5 @@ func routes(_ app: Application) throws {
         }
         return [String: [String: String]]()
                                      
-    app.get("users"){ req -> EventLoopFuture<View> in
-        
-        let u1 = User(id: 1, Q1: "Sometimes", Q2: "Never", Q3: "Fairly often")
-        let u2 = User(id: 2, Q1: "Fairly Often", Q2: "Almost Never", Q3: "Sometime")
-        let u3 = User(id: 3, Q1: "Almost Never", Q2: "Never", Q3: "Sometime")
-        let u4 = User(id: 4, Q1: "Never", Q2: "Almost Never", Q3: "Fairly often")
-       
-        return req.view.render("home", ["users": [u1, u2, u3, u4]])
-
-    }
+   
 }
