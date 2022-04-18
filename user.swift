b7483361c7ad62d3c449403075eb61f1884a98aa
arzhangv <34948438+arzhangv@users.noreply.github.com>
2022-04-18 04:10:44 -0700

    Add files via upload

---
+++ user.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Arzhang Valadkhani on 4/17/22.
+//
+
+import Foundation
+import Vapor
+
+struct User : Content{
+    var id: Int?
+    var Q1: String?
+    var Q2: String?
+    var Q3: String?
+}
+
+
+
